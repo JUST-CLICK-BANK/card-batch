22ab1409fa72e9f9b82f251f2e829c30f707c583
Sujin1517 <11tnwls12@gmail.com>
2024-08-05 12:09:23 +0900

    first commit

---
+++ src/main/java/com/click/cardbatch/CardbatchApplication.java
@@ -0,0 +1,13 @@
+package com.click.cardbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardbatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/click/cardbatch/CardbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.click.cardbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
